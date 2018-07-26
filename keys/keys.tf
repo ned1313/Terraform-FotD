1703908de60d83f018800005c9607a02d72a3982
Ned Bellavance <ned.bellavance@gmail.com>
2018-07-25 21:25:58 -0400

    add nested map

--- keys/keys.tf
+++ keys/keys.tf
@@ -20,6 +20,22 @@ variable "empty_map" {
   default = {}
 }
 
+variable "nested_map" {
+  type = "map"
+
+  default = {
+    "map_one" = {
+      "key_1-1" = "value_1-1"
+      "key_1-2" = "value_1-2"
+    }
+
+    "map_two" = {
+      "key_2-1" = "value_2-1"
+      "key_2-2" = "value_2-2"
+    }
+  }
+}
+
 ##############################################
 # Resources
 ##############################################
@@ -38,4 +54,8 @@ output "2_map_function_output" {
 
 output "3_empty_map_output" {
   value = "${keys(var.empty_map)}"
+}
+
+output "4_nested_map_output" {
+  value = "${keys(var.nested_map)}"
 }
